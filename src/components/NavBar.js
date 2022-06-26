d75d12b5e18fd4e7f0af0d06aaa3e1a1c4093f30
Md Mumtaj Alam <98735031+mumtajalam@users.noreply.github.com>
2022-06-26 21:12:27 +0530

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-
-export const NavBar = () =>{
-    return(
-
-    )
-}
+export const NavBar = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/" className="index-link">
+              Index
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/home" className="home-link">
+              Home
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
